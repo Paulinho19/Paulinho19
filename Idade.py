3ee75a63bb9f11ec142a9e1ad5243354fdfe992f
Paulinho19 <98109654+Paulinho19@users.noreply.github.com>
2022-02-06 14:31:12 -0300

    Add files via upload
    Programa que descobre sua idade

---
+++ Idade.py
@@ -0,0 +1,22 @@
+usuario = input ("Boa tarde. Qual seu nome? ")
+
+print (f"Olá {usuario}! Seu filho me programou para demonstrar todos os conhecimentos que ele está adiquirindo no curso de Engenharia de Computação!")
+
+print("Primeiramente vou fazer uma brincadeira com vc. Digite sua data de nascimento para eu descobrir sua idade. ")
+
+dia = input ("Q dia você nasceu? ")
+dia = int (dia)
+
+mes = input ("Agora me conte o mês: ")
+mes = int (mes)
+
+ano = input ("E o ano: ")
+ano = int (ano)
+
+print (f"Certo! então você nasceu em {dia}/{mes}/{ano} não é mesmo? ")
+input ()
+print (f"Já sei sua idade {usuario}! Quer que eu diga ou não? Hehe ")
+input ()
+print (f"Vou te dizer, mas você vai ficar me devendo um Pix, por que meu desenvolvedor quer sair com as gatinhas! Logo ele precisa de din din :) Ok?")
+input ()
+print (f"{usuario} tem {2022 - ano} anos. Um jovem senhor kkk. É isso {usuario}, agora vou me retirar pois estou cansada e preciso recarregar as energias. Até uma próxima execução.") 
