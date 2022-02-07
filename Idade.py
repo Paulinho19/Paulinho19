6a561d4afd36ca2a481975fc362c6fcbf22a4093
Paulo <98109654+Paulinho19@users.noreply.github.com>
2022-02-07 14:13:16 -0300

    Update Idade.py

--- Idade.py
+++ Idade.py
@@ -1,8 +1,8 @@
 usuario = input ("Boa tarde. Qual seu nome? ")
 
-print (f"Olá {usuario}! Seu filho me programou para demonstrar todos os conhecimentos que ele está adiquirindo no curso de Engenharia de Computação!")
+print (f"Olá {usuario}!\nO Paulo me programou para demonstrar todos os conhecimentos que ele está adiquirindo no curso de Engenharia de Computação!")
 
-print("Primeiramente vou fazer uma brincadeira com vc. Digite sua data de nascimento para eu descobrir sua idade. ")
+print("Primeiramente vou fazer uma brincadeira com vc.\nDigite algumas informações necessárias para que eu descubra sua idade. ")
 
 dia = input ("Q dia você nasceu? ")
 dia = int (dia)
@@ -17,6 +17,7 @@ print (f"Certo! então você nasceu em {dia}/{mes}/{ano} não é mesmo? ")
 input ()
 print (f"Já sei sua idade {usuario}! Quer que eu diga ou não? Hehe ")
 input ()
-print (f"Vou te dizer, mas você vai ficar me devendo um Pix, por que meu desenvolvedor quer sair com as gatinhas! Logo ele precisa de din din :) Ok?")
+print (f"Vou te dizer. Mas você vai ficar me devendo um Pix, pois meu desenvolvedor quer sair com as gatinhas! Logo ele precisa de din din :) Ok?")
 input ()
-print (f"{usuario} tem {2022 - ano} anos. Um jovem senhor kkk. É isso {usuario}, agora vou me retirar pois estou cansada e preciso recarregar as energias. Até uma próxima execução.") 
+print (f"{usuario} tem {2021 - ano} anos. Um jovem senhor kkk. É isso {usuario}, agora vou me retirar pois estou cansado e preciso recarregar as energias. Até uma próxima execução.") 
+
