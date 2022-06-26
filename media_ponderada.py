571680538367fa53b7c6683e23661ee9658104a6
Paulo <98109654+Paulinho19@users.noreply.github.com>
2022-06-26 11:51:32 -0300

    Add files via upload

---
+++ media_ponderada.py
@@ -0,0 +1,10 @@
+# O programa realiza a operação de média ponderada de 5 entradas
+m1 = float(input())
+m2 = float(input())
+m3 = float(input())
+m4 = float(input())
+m5 = float(input())
+
+result = (m1*1+m2*2+m3*3+m4*4+m5*5)/15
+
+print("%.3f"%result)
